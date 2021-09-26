6aef862c1baa5157a12ed83ef7b113b92725c285
Sergey <seregahromoi@icloud.com>
2021-09-26 12:51:59 +0400

    Initial Commit

---
+++ Recipe search(Clean+MVVM)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recipe search(Clean+MVVM)
+//
+//  Created by Sergey on 26.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
