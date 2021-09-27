302c8761274d73b60c09730fabadabb4a194b203
Sergey <seregahromoi@icloud.com>
2021-09-27 14:47:52 +0400

    NetWork

---
+++ Recipe search(Clean+MVVM)/Application/APPConfiguration.swift
@@ -0,0 +1,29 @@
+//
+//  APPConfiguration.swift
+//  Recipe search(Clean+MVVM)
+//
+//  Created by Sergey on 26.09.2021.
+//
+
+import Foundation
+
+final class AppConfiguration {
+    lazy var apiKey: String = {
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "ApiKey") as? String else {
+            fatalError("ApiKey must not be empty in plist")
+        }
+        return apiKey
+    }()
+    lazy var apiBaseURL: String = {
+        guard let apiBaseURL = Bundle.main.object(forInfoDictionaryKey: "ApiBaseURL") as? String else {
+            fatalError("ApiBaseURL must not be empty in plist")
+        }
+        return apiBaseURL
+    }()
+    lazy var imagesBaseURL: String = {
+        guard let imageBaseURL = Bundle.main.object(forInfoDictionaryKey: "ImageBaseURL") as? String else {
+            fatalError("ApiBaseURL must not be empty in plist")
+        }
+        return imageBaseURL
+    }()
+}
